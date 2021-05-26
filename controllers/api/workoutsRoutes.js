4352e6874bda5ba9307858a99a3643c008fb3364
Riheel Hamoande <riheel83@gmail.com>
2021-05-25 19:05:31 -0500

    Redirect to stats, excercise

--- controllers/api/workoutsRoutes.js
+++ controllers/api/workoutsRoutes.js
@@ -1,12 +1,27 @@
 const router = require('express').Router();
-// const { Workout } = require('../../models');
+const { Workout } = require('../../models');
 const db = require("../../models");
 
-router.get("/", (req, res) => {
-    db.Workout.find({})
-    .then(data => {
-        res.json(data)
-    })
+
+// router.get("/", (req, res) => {
+//     db.Workout.find({})
+//     .then(data => {
+//         res.json(data)
+//     })
+// })
+
+router.get("/", async(req, res) => {
+    const workoutData = await Workout.find({})
+    res.send(workoutData)
 })
+
+// router.post("/", async (req, res) => {
+//     db.Workout.create(req.body)
+// })
+
+// router.post("/", async(req, res) => {
+//     const workData = await Workout.create({body})
+//     res.send(workData)
+// })
 
 module.exports = router;
---
+++ controllers/homeRoutes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+// const { Workout } = require('../models');
+
+router.get("/exercise", async (req, res)=> {
+    await res.redirect('./exercise.html')
+})
+
+router.get("/stats", async (req, res)=> {
+    await res.redirect('./stats.html')
+})
+
+module.exports = router;
