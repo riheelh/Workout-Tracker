f1ba3aa43455b36ebcd28f99a137596ed72fe89d
Riheel Hamoande <riheel83@gmail.com>
2021-05-31 16:32:04 -0500

    fix date in model

--- models/Workout.js
+++ models/Workout.js
@@ -4,7 +4,7 @@ const Schema =  mongoose.Schema;
 const WorkoutSchema = new Schema ({
     day: {
         type: Date,
-        default: new Date().getDate()
+        default: new Date(new Date().setDate(new Date().getDate()))
     },
     exercises: [
         {
